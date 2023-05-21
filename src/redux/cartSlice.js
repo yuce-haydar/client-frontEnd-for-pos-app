1bb4d3556a76ae7f13195f8fb737c0d8146e281c
HAYDAR <HAYDAR@DESKTOP-7DG9GQL>
2023-05-21 23:49:08 +0300

    redux basliyorum

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice=createSlice({
+    name:'cart',
+    initialState:{
+        cartItems:[],
+        total:0
+    },
+    reducers:{
+
+    }
+})
+export default cartSlice.reducer;
